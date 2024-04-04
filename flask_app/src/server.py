ad0da62a551e1879a9126f5fb4b497c75f22b923
Ieso Dias <iesodias@gmail.com>
2024-04-04 12:15:33 -0300

    automatic commit

---
+++ flask_app/src/server.py
@@ -0,0 +1,14 @@
+import socket
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return render_template('index.html', id_container=socket.gethostname())
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
